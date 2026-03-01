840f6032357e7caf6c2ce3836e0d7ac97d4c3b7d
tyron <wishsmp1002@gmail.com>
2026-03-01 17:27:00 +1100

    Initial Commit

--- src/main/java/net/saturn/antiHealth/AntiHealth.java
+++ src/main/java/net/saturn/antiHealth/AntiHealth.java
@@ -1,12 +1,19 @@
 package net.saturn.antiHealth;
 
+import net.minecraft.network.protocol.game.ServerboundChangeGameModePacket;
+import net.minecraft.server.level.ServerEntity;
+import net.minecraft.server.level.ServerPlayer;
+import org.bukkit.Bukkit;
+import org.bukkit.craftbukkit.v1_21_R7.entity.CraftPlayer;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class AntiHealth extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
+        // Plugin startup logic\
+
+
 
     }
 
